eee23d91dc83a9866937e8ae5bb247111a5c8f00
EricOliveiraSampaio <ericaosampaio25@gmail.com>
2024-11-27 09:12:05 -0300

    Add project files.

---
+++ appLogin/Libraries/Login/LoginCliente.cs
@@ -0,0 +1,22 @@
+﻿using appLogin.Models;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+
+namespace appLogin.Libraries.Login
+{
+    public class LoginCliente
+    {
+        private string Key = "Login.Cliente";
+        private Sessao.Sessao _sessao;
+        public LoginCliente(Sessao.Sessao sessao)
+        {
+            _sessao = sessao;
+        }
+        public void Login(Cliente cliente )
+        {
+            string clienteJSONString = JsonConvert.SerializeObject( cliente ); 
+
+            _sessao.Cadastrar(Key, clienteJSONString);
+
+        }
+    }
+}
---
+++ appLogin/Program.cs
@@ -0,0 +1,46 @@
+using appLogin.Repository;
+using appLogin.Repository.Contract;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Adicionar a interface como um serviço 
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IColaboradorRepository, ColaboradorRepository>();
+
+builder.Services.AddScoped <appLogin.Libraries.Sessao.Sessao>();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(60);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddMvc().AddSessionStateTempDataProvider();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCookiePolicy();
+app.UseDefaultFiles();
+app.UseSession();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
